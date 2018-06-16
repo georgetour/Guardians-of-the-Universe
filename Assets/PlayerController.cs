492025074b8d9a5c66ebd4b9f9d4731720a6bdaa
George Tourtsinakis <george@besmartbesimple.com>
2018-06-16 20:19:17 +0300

    Added movement for player particles  and background
    TODO Make the ship movement to z axis with some roation also

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Move speed
+    public float speed = 5f;
+    public float health = 500;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithArrows();
+    }
+
+    //Move playership
+    private void MoveWithArrows()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+
+
+    }
+}
